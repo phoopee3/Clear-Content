9033155466dd2db175958ae395772b79aba78941
jason lawton <jason@jasonlawton.com>
2021-05-24 20:01:12 -0400

    code cleanup and sanitize inputs

--- clear-content.php
+++ clear-content.php
@@ -2,21 +2,18 @@
 /**
  * Plugin Name: Clear Content
  * Description: Clear content based on post type
- * Version: 1
+ * Version: 1.0.0
  * Author: Jason Lawton <jason@jasonlawton.com>
  */
 
-define( 'JHL_CP_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-define( 'JHL_CP_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-
 include( 'inc/api.php' );
 
-add_action( 'admin_menu', 'jhl_cp_add_admin_menu' );
+add_action( 'admin_menu', 'jhl_cc_add_admin_menu' );
 
-function jhl_cp_add_admin_menu() {
-    add_menu_path( 'Clear Content', 'Clear Content', 'manage_categories', 'clear_content', 'jhl_cp_options_page' );
+function jhl_cc_add_admin_menu() {
+    add_menu_path( 'Clear Content', 'Clear Content', 'manage_categories', 'clear_content', 'jhl_cc_options_page' );
 }
 
-function jhl_cp_options_page() {
+function jhl_cc_options_page() {
     include 'options-form.php';
 }
--- inc/api.php
+++ inc/api.php
@@ -3,20 +3,29 @@ add_action( 'wp_ajax_clear_content', 'jhl_cp_clear_content' );
 add_action( 'wp_ajax_nopriv_clear_content', 'jhl_cp_clear_content' );
 
 function jhl_cp_clear_content() {
-    $post = $_POST['post_id'];
-    if ( $_POST['remove_attachments'] ) {
-        $children = get_children( [ 'post_parent' => $post_id, 'fields' => 'ids' ] );
-        foreach( $children as $child ) {
-            $result = wp_delete_attachment( $child, true );
-        }
-    }
-    $result = wp_delete_post( $post_id, true );
+    $post_id = sanitize_text_field( $_POST['post_id'] );
 
     header( 'Content-Type:application/json' );
-    echo json_encode( [
-        'success' => ( $data !== false ),
-        'data'    => $post_id,
-    ] );
+
+    if ( $post_id && is_numeric( $post_id ) ) {
+        if ( $_POST['remove_attachments'] ) {
+            $children = get_children( [ 'post_parent' => $post_id, 'fields' => 'ids' ] );
+            foreach( $children as $child ) {
+                $result = wp_delete_attachment( $child, true );
+            }
+        }
+        $result = wp_delete_post( $post_id, true );
+
+        echo json_encode( [
+            'success' => ( $data !== false ),
+            'data'    => $post_id,
+        ] );
+    } else {
+        echo json_encode( [
+            'success' => false,
+            'data'    => 'Invalid post id',
+        ] );
+    }
     die;
 }
 
@@ -24,24 +33,30 @@ add_action( 'wp_ajax_get_post_ids', 'jhl_cp_get_post_ids' );
 add_action( 'wp_ajax_nopriv_get_post_ids', 'jhl_cp_get_post_ids' );
 
 function jhl_cp_get_post_ids() {
-    $post_type = $_POST['post_type'];
-    $post_ids = new WP_Query( [
-        'post_type' => $post_type,
-        'posts_per_page' => -1,
-        'fields' => 'ids',
-        'post_status' => 'any',
-    ] );
-
+    $post_type = sanitize_text_field( $_POST['post_type'] );
     header( 'Content-Type:application/json' );
+    if ( $post_type ) {
+        $post_ids = new WP_Query( [
+            'post_type' => $post_type,
+            'posts_per_page' => -1,
+            'fields' => 'ids',
+            'post_status' => 'any',
+        ] );
 
-    if ( $post_ids->found_posts ) {
-        $post_ids = $post_ids->posts;
+        if ( $post_ids->found_posts ) {
+            $post_ids = $post_ids->posts;
+        } else {
+            $post_ids = [];
+        }
+        echo json_encode( [
+            'success' => ( $data !== false ),
+            'data' => $post_ids,
+        ] );
     } else {
-        $post_ids = [];
+        echo json_encode( [
+            'success' => false,
+            'data' => 'Invalid post type',
+        ] );
     }
-    echo json_encode( [
-        'success' => ( $data !== false ),
-        'data' => $post_ids,
-    ] );
     die;
 }
