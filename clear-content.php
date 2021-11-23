abb4f8a0815b2db21a6133e9c096f1670e81dab3
Jason Lawton <jason@jasonlawton.com>
2021-11-23 14:30:32 -0500

    Add trash icon to menu entry

--- clear-content.php
+++ clear-content.php
@@ -11,7 +11,7 @@ include( 'inc/api.php' );
 add_action( 'admin_menu', 'jhl_cc_add_admin_menu' );
 
 function jhl_cc_add_admin_menu() {
-    add_menu_page( 'Clear Content', 'Clear Content', 'manage_categories', 'clear_content', 'jhl_cc_options_page' );
+    add_menu_page( 'Clear Content', 'Clear Content', 'manage_categories', 'clear_content', 'jhl_cc_options_page', 'dashicons-trash' );
 }
 
 function jhl_cc_options_page() {
